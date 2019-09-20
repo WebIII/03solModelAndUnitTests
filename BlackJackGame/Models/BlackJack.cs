2b2658d9606e3daaf95416004a9e3bc8a7869e38
Karine Samyn <Karine.Samyn@hogent.be>
2019-09-20 14:29:15 +0200

    Implement BlackJack and BlackJackTest

--- BlackJackGame/Models/BlackJack.cs
+++ BlackJackGame/Models/BlackJack.cs
@@ -4,7 +4,6 @@ namespace BlackJackGame.Models
 {
     public class BlackJack
     {
-
         #region Fields
         public const bool FaceDown = false;
         public const bool FaceUp = true;
@@ -18,50 +17,83 @@ namespace BlackJackGame.Models
         #endregion
 
         #region Constructors
-        public BlackJack()
-        {
-            throw new NotImplementedException();
-        }
+        public BlackJack() : this(new Deck()) { }
 
         public BlackJack(Deck deck)
         {
-            throw new NotImplementedException();
+            _deck = deck;
+            DealerHand = new Hand();
+            PlayerHand = new Hand();
+            Deal();
         }
-
         #endregion
 
         #region Methods
         private void Deal()
         {
-            throw new NotImplementedException();
+            AddCardToHand(DealerHand, FaceUp);
+            AddCardToHand(DealerHand, FaceDown);
+            AddCardToHand(PlayerHand, FaceUp);
+            AddCardToHand(PlayerHand, FaceUp);
+            AdjustGameState(GameState.PlayerPlays);
         }
 
         public void GivePlayerAnotherCard()
         {
-            throw new NotImplementedException();
+            if (GameState != GameState.PlayerPlays)
+                throw new InvalidOperationException("You cannot take a card now...");
+            AddCardToHand(PlayerHand, FaceUp);
+            AdjustGameState();
         }
 
         public void PassToDealer()
         {
-            throw new NotImplementedException();
+            DealerHand.TurnAllCardsFaceUp();
+            AdjustGameState(GameState.DealerPlays);
+            LetDealerFinalize();
         }
         private void LetDealerFinalize()
         {
-            throw new NotImplementedException();
+            while (GameState == GameState.DealerPlays)
+            {
+                AddCardToHand(DealerHand, FaceUp);
+                AdjustGameState();
+            }
         }
 
         private void AdjustGameState(GameState? gameState = null)
         {
-            throw new NotImplementedException();
+            if (gameState.HasValue)
+                GameState = gameState.Value;
+            if (GameState == GameState.PlayerPlays && PlayerHand.Value >= 21)
+                PassToDealer();
+            if (GameState == GameState.DealerPlays && (PlayerHand.Value > 21 || DealerHand.Value >= 21 || DealerHand.Value >= PlayerHand.Value))
+                GameState = GameState.GameOver;
         }
+
         private void AddCardToHand(Hand hand, bool faceUp)
         {
-            throw new NotImplementedException();
+            BlackJackCard card = _deck.Draw();
+            if (faceUp)
+                card.TurnCard();
+            hand.AddCard(card);
         }
 
         public string GameSummary()
         {
-            throw new NotImplementedException();
+            if (GameState != GameState.GameOver)
+                return null;
+            if (PlayerHand.Value > 21)
+                return "Player burned, dealer wins";
+            if (PlayerHand.Value == 21 && PlayerHand.NrOfCards == 2 && DealerHand.Value != 21)
+                return "BLACKJACK";
+            if (PlayerHand.Value == DealerHand.Value)
+                return "Equal, dealer wins";
+            if (DealerHand.Value > 21)
+                return "Dealer burned, player wins";
+            if (DealerHand.Value > PlayerHand.Value)
+                return "Dealer wins";
+            return "Player wins";
         }
         #endregion
     }
